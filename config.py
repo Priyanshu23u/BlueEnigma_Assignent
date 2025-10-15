92c363593ff9685c7f5472cc351af8cc0dcc0df5
Priyanshu23u <upriyanshujnp@gmail.com>
2025-10-15 23:39:50 +0530

    Initial Setup Complete

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv()
+
+# Neo4j
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
+AURA_INSTANCEID = os.getenv("AURA_INSTANCEID")
+AURA_INSTANCENAME = os.getenv("AURA_INSTANCENAME")
+
+# Groq
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# Pinecone
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+PINECONE_VECTOR_DIM = int(os.getenv("PINECONE_VECTOR_DIM", 1536))
+PINECONE_EMBED_MODEL = os.getenv("PINECONE_EMBED_MODEL")
+PINECONE_FIELD_MAP = dict([kv.split(":") for kv in os.getenv("PINECONE_FIELD_MAP","text:chunk_text").split(",")])
